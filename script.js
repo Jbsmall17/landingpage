bbbce15ee16243421f1a4062daa79afc5533a73d
Jbsmall17 <alaomiyaki17@gmail.com>
2023-07-08 19:24:51 -0700

    Added an accordion

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const openIcon = document.querySelector(".menu")
 const closeIcon = document.querySelector(".menu.close")
 const headerEl = document.querySelector(".primary-header")
-
+const nodelistEl = document.querySelectorAll("div.main-div div.div-second div")
 openIcon.addEventListener("click",()=>{
     headerEl.classList.add("active")
 })
 closeIcon.addEventListener("click",()=>{
     headerEl.classList.remove("active")
+})
+
+nodelistEl.forEach((nodeEl)=>{
+    nodeEl.addEventListener("click",(e)=>{
+        e.target.parentElement.classList.toggle("active")
+    })
 })
